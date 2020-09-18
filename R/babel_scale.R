bd1b0a56f25e732810ec99e386f0a24877a554f6
Julian Bernauer <julian.bernauer@mzes-uni-mannheim.de>
2020-09-18 16:49:56 +0200

    Init of function babel_scale

---
+++ R/babel_scale.R
@@ -0,0 +1,35 @@
+# You can learn more about package authoring with RStudio at:
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' @title babel_scale
+#'
+#' @description This function scales across text.
+#'
+#' @param dat Your dataset
+#' @param id Your scope ("e.g. text or individual IDs")
+#' @param time Your time
+#' @return
+#' @examples
+#' @export
+#' @importFrom dplyr "%>%"
+#' @importFrom ggplot2 ggplot
+babel_scale <-
+  function(dat,
+           id,
+           time,
+           ) {
+
+    return(plot_scaling)
+
+  }
+
+
+
+
+
